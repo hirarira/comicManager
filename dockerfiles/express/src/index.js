4f1cc82e54353d71d45ff05b9a6078dd4b16e305
hirarira <hirarira617@gmail.com>
2020-07-24 23:58:11 +0900

    expressサーバーの基礎を追加しました

---
+++ dockerfiles/express/src/index.js
@@ -0,0 +1,36 @@
+"use strict";
+const Express = require("express");
+const app = Express();
+const BodyParser = require('body-parser');
+
+const Sequelize = require('sequelize');
+
+// urlencodedとjsonは別々に初期化する
+app.use(BodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(BodyParser.json());
+
+const server = app.listen(3333, function(){
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.get("/status", (req, res)=>{
+  const testBody = {
+    'status': 'ok',
+    'message': 'status ok'
+  }
+  res.header('Content-Type', 'application/json');
+  res.send(testBody);
+});
+
+// 404
+app.use(function(req, res, next) {
+  res.header('Content-Type', 'application/json');
+  let res_body = {
+    status: 'ng',
+    body: '404 not found'
+  };
+  res.status(404).send(res_body);
+});
