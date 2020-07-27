f326659af432f1084353e7474f720d0cb8a57a15
hirarira <hirarira617@gmail.com>
2020-07-28 01:34:13 +0900

    漫画一覧を取得して選択できるようにしました

--- dockerfiles/react/src/api/comics.ts
+++ dockerfiles/react/src/api/comics.ts
@@ -1,13 +1,14 @@
+import axios from 'axios';
+
 class Comics {
   host: string;
   axios: any;
   constructor() {
     this.host = "http://localhost:3333";
-    this.axios = require('axios');
   }
   async getComicList() {
     const url = this.host + '/get/comicList';
-    return await this.axios.get(url);
+    return await axios.get(url);
   }
 }
 
--- dockerfiles/react/src/pages/home.tsx
+++ dockerfiles/react/src/pages/home.tsx
@@ -1,8 +1,8 @@
-import { FC, useEffect, useCallback } from "react";
+import { FC, useEffect, useCallback, useState } from "react";
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Header from "../components/Header";
-import { Grid } from "@material-ui/core";
+import { Grid, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
 import Comics from '../api/comics';
 
 interface Props {
@@ -12,20 +12,33 @@ interface Props {
 const useStyles = makeStyles((theme) => ({
   root: {
     textAlign: 'center'
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300
   }
 }));
 
 const Home: FC<Props> = (()=>{
   const classes = useStyles();
-  const comics = new Comics();
-  let comicList;
+  const [comicList, setComicList] = useState([]);
+  const [selectComic, setSelectComic] = useState('');
+
+  const handleComicChange = (event: any) => {
+    setSelectComic(event.target.value);
+  };
+
   const fetch = useCallback(async()=>{
+    const comics = new Comics();
     const getComicsList = await comics.getComicList();
-    console.log(getComicsList);
-  }, [comicList]);
+    setComicList(getComicsList.data.body);
+  }, []);
+
   useEffect(()=>{
     fetch();
   }, [fetch]);
+
   return (
     <div>
       <Header />
@@ -35,7 +48,19 @@ const Home: FC<Props> = (()=>{
           漫画検索
         </Grid>
         <Grid item xs={8}>
-          漫画検索
+          <FormControl className={classes.formControl}>
+            <InputLabel id="comic_title_label">漫画タイトル</InputLabel>
+            <Select
+              value={selectComic}
+              onChange={handleComicChange}
+            >
+              {comicList.map((x: any)=>{
+                return (
+                  <MenuItem key={x.id} value={x.id}>{x.title}</MenuItem >
+                )
+              })}
+            </Select>
+          </FormControl>
         </Grid>
         <Grid item xs={4}>
           検索
