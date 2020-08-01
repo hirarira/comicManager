060fc603e125e615f012aab11fa96d00b6c5f2b8
hirarira <hirarira617@gmail.com>
2020-08-01 13:52:27 +0900

    Comic詳細を取得します

--- dockerfiles/react/src/api/comics.ts
+++ dockerfiles/react/src/api/comics.ts
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 class Comics {
   host: string;
-  axios: any;
   constructor() {
     this.host = "http://localhost:3333";
   }
   async getComicList() {
     const url = this.host + '/get/comicList';
+    return await axios.get(url);
+  }
+  async getComicDetail(id: string) {
+    const url = this.host + '/get/comic/' + id;
     return await axios.get(url);
   }
 }
--- dockerfiles/react/src/pages/comicDetail.tsx
+++ dockerfiles/react/src/pages/comicDetail.tsx
@@ -1,18 +1,56 @@
-import { FC } from "react";
+import React, { FC, useCallback, useState, useEffect } from "react";
 import Header from "../components/Header";
-import React from "react";
 import { RouteComponentProps } from 'react-router-dom'
+import Comics from '../api/comics';
+import { makeStyles, Grid } from "@material-ui/core";
+import { ComicDetailFormat, initComicDetail } from "../type/ComicDetail";
 
 type DetailProps = RouteComponentProps<{
   comicID: string
 }>
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center'
+  },
+  paperArea: {
+    margin: "20px",
+    padding: "20px",
+    width: "640px"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 240
+  }
+}));
+
 const ComicDetail: FC<DetailProps> = ((props)=>{
+  const classes = useStyles();
+  const comicID = props.match.params.comicID;
+  const [comic, setComic] = useState({});
+  const fetch = useCallback(async()=>{
+    const comics = new Comics();
+    const comicDetail = await comics.getComicDetail(comicID);
+    setComic(comicDetail.data.body);
+  }, []);
+
+  useEffect(()=>{
+    fetch();
+  }, [fetch]);
+  
   return (
     <div>
       <Header/>
       漫画詳細ページ
       {props.match.params.comicID}
+      <Grid container justify="center" className={classes.root}>
+        <Grid item xs={6}>
+        </Grid>
+        <Grid item xs={6}>
+          書影
+        </Grid>
+      </Grid>
     </div>
   )
 });
