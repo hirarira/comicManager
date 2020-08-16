b871d1602255e52fa2c5be5bdfc4470ec6679a1a
hirarira <hirarira617@gmail.com>
2020-08-16 17:55:08 +0900

    購入フラグを切り替えられるようにした

--- dockerfiles/react/src/pages/createComicInfo.tsx
+++ dockerfiles/react/src/pages/createComicInfo.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useCallback, useState, useEffect } from "react";
 import Header from "../components/Header";
 import Comics from '../api/comics';
-import { makeStyles, Grid, Button } from "@material-ui/core";
+import { makeStyles, Grid, Button, TableContainer, Table, TableBody, TableRow, TableCell, Paper, FormControl, Select, MenuItem } from "@material-ui/core";
 import { initComicDetail } from "../type/ComicDetail";
 import ComicAboutTable from "../components/ComicAboutTable";
 
@@ -15,6 +15,8 @@ const useStyles = makeStyles(() => ({
     width: "640px"
   },
   table: {
+    marginLeft: "auto",
+    marginRight: "auto",
     margin: "20px",
     maxWidth: "640px"
   },
@@ -35,11 +37,12 @@ const CreateComicInfo: FC<any> = ((props)=>{
   const query = qs.parse(queryStr);
   */
   const params = props.match.params;
-  console.log(params);
   const classes = useStyles();
 
   const [comic, setComic] = useState<any>(initComicDetail);
+  const [comicDetail, setComicDetail] = useState<any>(null);
   const [isShowPage, setIsShowPage] = useState(false);
+  const [buyFlag, setBuyFlag] = useState<string>("");
   const fetch = useCallback(async()=>{
     // queryにcomicIDがないとエラーにする
     // const comicID: string = typeof query.comicID === "string"? query.comicID: '';
@@ -50,9 +53,22 @@ const CreateComicInfo: FC<any> = ((props)=>{
       const comics = new Comics();
       const comicDetail = await comics.getComicDetail(comicID);
       setComic(comicDetail.data.body);
-      console.log(comicDetail);
+      const detail = comicDetail.data.body.detail.find((x: any)=>{
+        return x.number == volID;
+      });
+      console.log(detail);
+      if(detail) {
+        setComicDetail(detail);
+        setBuyFlag(detail.info.buyFlag);
+      } else {
+        setComicDetail(null);
+      }
     }
   }, []);
+
+  const submitRegistVol = () => {
+    console.log(buyFlag);
+  }
 
   useEffect(()=>{
     fetch();
@@ -61,7 +77,7 @@ const CreateComicInfo: FC<any> = ((props)=>{
   return (
     <div>
       <Header/>
-      {isShowPage &&
+      {isShowPage && comicDetail &&
         <Grid container justify="center" className={classes.root}>
           <Grid item xs={6}>
             <ComicAboutTable
@@ -77,13 +93,55 @@ const CreateComicInfo: FC<any> = ((props)=>{
             <p className={classes.subtitle}>漫画既読・コメント登録</p>
           </Grid>
           <Grid item xs={12}>
-            <Button size="large" variant="contained" color="primary">
+            <FormControl>
+              <TableContainer component={Paper} className={classes.table}>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>巻数</TableCell>
+                      <TableCell>{comicDetail.number}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>購入フラグ</TableCell>
+                      <TableCell>
+                        <Select
+                          value={buyFlag}
+                          onChange={(x)=>{ setBuyFlag(String(x.target.value)) }}
+                        >
+                          <MenuItem value={"true"}>True</MenuItem>
+                          <MenuItem value={"false"}>False</MenuItem>
+                        </Select>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>購入日</TableCell>
+                      <TableCell>{comicDetail.info.buyDate}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>読破フラグ</TableCell>
+                      <TableCell>{String(comicDetail.info.readFlag)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>読破日</TableCell>
+                      <TableCell>{comicDetail.info.readDate}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>コメント</TableCell>
+                      <TableCell>{comicDetail.info.comment}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <Button size="large" variant="contained" color="primary" onClick={ submitRegistVol }>
               漫画既読登録
             </Button>
           </Grid>
         </Grid>
       }
-      {!isShowPage &&
+      {!isShowPage || !comicDetail &&
         <Grid container justify="center" className={classes.root}>
           <Grid item xs={12}>
             <p className={classes.subtitle}>comicIDがありません！</p>
