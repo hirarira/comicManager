58c5f2ec998404e98e400e362ed62267674040ae
hirarira <hirarira617@gmail.com>
2020-11-03 22:10:33 +0900

    作者の追加ができるように修正しました

--- dockerfiles/react/src/pages/authotManager.tsx
+++ dockerfiles/react/src/pages/authotManager.tsx
@@ -1,7 +1,8 @@
-import { Grid, makeStyles, Paper } from "@material-ui/core";
+import { Button, Grid, makeStyles, MenuItem, Paper, Select, TextField } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import React, { FC, useEffect, useCallback, useState } from "react";
 import Authors from "../api/authors";
-import AuthorListTable from "../components/AuthorListTable";
+import AuthorListTable, { Author } from "../components/AuthorListTable";
 import Header from "../components/Header";
 
 const AuthorManager: FC = (()=>{
@@ -28,6 +29,12 @@ const AuthorManager: FC = (()=>{
   }));
   const classes = useStyles();
   const [authorList, setAuthorList] = useState([]);
+  const [createAuthorName, setCreateAuthorName] = useState("");
+  const [deleteAuthorID, setDeleteAuthorID] = useState(0);
+  const [httpResStatus, setHttpResStates] = useState({
+    status: '',
+    message: ''
+  });
 
   const authors = new Authors();
 
@@ -40,6 +47,22 @@ const AuthorManager: FC = (()=>{
     document.title = '漫画管理アプリ: 作者管理';
     fetch();
   }, [fetch]);
+
+  const submitCreateAuthor = async () =>{
+    try {
+      const res = await authors.createAuthor(createAuthorName);
+      setHttpResStates({
+        status: 'ok',
+        message: '正常に作者が追加されました'
+      });
+    } catch (e) {
+      setHttpResStates({
+        status: 'ng',
+        message: '作者が追加されませんでした'
+      });
+    }
+    fetch();
+  }
   
   return (
     <div>
@@ -57,13 +80,48 @@ const AuthorManager: FC = (()=>{
         </Paper>
         <Paper className={classes.paperArea}>
           <Grid item xs={12}>
-              作者作成
+            作者作成
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              value={createAuthorName}
+              onChange={ (e)=>{ setCreateAuthorName(e.target.value) } }
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button size="large" variant="contained" color="primary" onClick={ submitCreateAuthor }>
+              作者新規登録
+            </Button>
           </Grid>
         </Paper>
         <Paper className={classes.paperArea}>
           <Grid item xs={12}>
               作者削除
           </Grid>
+          <Grid item xs={12}>
+            <Select
+              value={deleteAuthorID}
+              onChange={(x)=>{ setDeleteAuthorID(Number(x.target.value)) }}
+            >
+              {authorList.map((author: Author, id: number)=>{
+                return (
+                  <MenuItem key={id} value={author.id}>{author.id}: {author.name}</MenuItem>
+                )
+              })}
+            </Select>
+          </Grid>
+        </Paper>
+        <Paper className={classes.paperArea}>
+          {httpResStatus.status === 'ok' &&
+            <Alert severity="success">
+              {httpResStatus.message}
+            </Alert>
+          }
+          {httpResStatus.status === 'ng' &&
+            <Alert severity="error">
+              {httpResStatus.message}
+            </Alert>
+          }
         </Paper>
       </Grid>
     </div>
